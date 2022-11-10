61f4f5afa943688554a5ce30cb3c2e0d50134350
Sam <samuelamorim811@gmail.com>
2022-11-09 21:06:47 -0300

    09/11/2022 - 21:06

--- src/DonutManagemant/Donut.java
+++ src/DonutManagemant/Donut.java
@@ -5,35 +5,92 @@ public class Donut implements ManagerInterface {
 	private int id;
 	private String nome;
 	private String cobertura;
-	private int preço;
+	private int preco;
 	private String recheio;
 	private String tamanho;
 	
 	
 	public Donut() {
-		
 	}
 	
+	public Donut(String nome, String Cobertura, int preco, String recheio, String tamanho) {
+		this.nome = nome;
+		this.cobertura = Cobertura;
+		this.preco = preco;
+		this.recheio = recheio;
+	}
+	
+	
+	//Setters and getters
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCobertura() {
+		return cobertura;
+	}
+
+	public void setCobertura(String cobertura) {
+		this.cobertura = cobertura;
+	}
+
+	public int getPreco() {
+		return preco;
+	}
+
+	public void setPreco(int preco) {
+		this.preco = preco;
+	}
+
+	public String getRecheio() {
+		return recheio;
+	}
+
+	public void setRecheio(String recheio) {
+		this.recheio = recheio;
+	}
+
+	public String getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(String tamanho) {
+		this.tamanho = tamanho;
+	}
+	
+	
+	// metodo para inserir objetos
 	@Override
 	public void insertObject() {
 		
 		
 	}
+
+	//metodo para remover objetos por id
 	@Override
 	public void removeObject() {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	//metodo para atualizar um atributo por id
 	@Override
 	public void updateObject() {
 		// TODO Auto-generated method stub
 		
 	}
+	//metodo para retornar tudo sobre o objeto
 	@Override
 	public void returnObject() {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	//metodo que retorna a posição do objeto no array
 	@Override
 	public int returnObjectIndex() {
 		// TODO Auto-generated method stub
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,12 @@ import DonutManagemant.*;
 public class Main {
 
 	public static void main(String[] args) {
-
+	
+		Donut[] donut = new Donut[2];
 		
+		donut[1] = new Donut("jorge", "a", 123, "a", "a");
+		
+		System.out.println(donut[1]);
 
 	}
 
