343252a0ace5e9577e9e598753b232732190b58a
Sam <samuelamorim811@gmail.com>
2022-11-09 21:09:06 -0300

    09/11/2022 -> 21:09

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 	
-		Donut[] donut = new Donut[2];
 		
-		donut[1] = new Donut("jorge", "a", 123, "a", "a");
-		
-		System.out.println(donut[1]);
+		Donut donut = new Donut();
 
 	}
 
